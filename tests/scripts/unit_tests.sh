a614c4661e6e31f2b9e05ea139003b689c5a7748
Scott Piper <scott@summitroute.com>
2020-01-09 12:11:42 -0700

    Tested and documented how to have unit tests for your private auditors

--- tests/scripts/unit_tests.sh
+++ tests/scripts/unit_tests.sh
@@ -3,10 +3,17 @@ if [ -f .coverage ]; then
   rm .coverage
 fi
 
-python3 -m "nose" tests/unit \
+export PRIVATE_TESTS=""
+echo "echo"
+echo $PRIVATE_TESTS
+if [ -d parliament/private_auditors/tests/ ]; then
+  export PRIVATE_TESTS="parliament/private_auditors/tests/"
+fi
+
+python3 -m "nose" tests/unit $PRIVATE_TESTS \
 --with-coverage \
 --cover-package=parliament \
 --cover-html \
+--cover-min-percentage=75 \
 --cover-html-dir=htmlcov
 
-# --cover-min-percentage=60 \

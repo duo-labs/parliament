ff1d8ae5ffa4aa09cc728a758b242c5c3190a9f3
Scott Piper <scott@summitroute.com>
2020-06-08 16:18:47 -0600

    Require python 3.6 or greater due to f-strings

--- setup.py
+++ setup.py
@@ -44,11 +44,10 @@ setup(
     zip_safe=True,
     license="BSD 3",
     keywords="aws parliament iam lint audit",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
